5872390085fb4247027842ab9a9ce984b3cee4f9
MichalStec01 <81973503+MichalStec01@users.noreply.github.com>
2021-06-13 12:41:58 +0000

    Deploying to gh-pages from @ MichalStec01/team_5-project@c8f47a2e4ea47c4bf42c54ce53807d3179399da8 🚀

--- src.bef1b092.js
+++ src.bef1b092.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team_5-project/src.1d3ba378.js.map
+//# sourceMappingURL=/team_5-project/src.bef1b092.js.map
